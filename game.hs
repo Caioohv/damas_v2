2b92835afeb0f4c1734dc4941c3e765ffe76afc7
Caio Vieira <caioviier@gmail.com>
2025-06-27 22:00:30 -0300

    feat: implement board initialization and display functions

--- game.hs
+++ game.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.List (intercalate)
+
 data Piece = Pawn | King deriving (Eq, Show)
 data Player = Red | Black deriving (Eq, Show)
 data Square = Empty | Occupied Player Piece deriving (Eq, Show)
@@ -13,6 +15,41 @@ data GameState = GameState
     , gameOver :: Bool
     , winner :: Maybe Player
     } deriving (Show)
+
+initialBoard :: Board
+initialBoard = 
+    [ [Empty, Occupied Black Pawn, Empty, Occupied Black Pawn, Empty, Occupied Black Pawn, Empty, Occupied Black Pawn]
+    , [Occupied Black Pawn, Empty, Occupied Black Pawn, Empty, Occupied Black Pawn, Empty, Occupied Black Pawn, Empty]
+    , [Empty, Occupied Black Pawn, Empty, Occupied Black Pawn, Empty, Occupied Black Pawn, Empty, Occupied Black Pawn]
+    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+    , [Empty, Empty, Empty, Empty, Empty, Empty, Empty, Empty]
+    , [Occupied Red Pawn, Empty, Occupied Red Pawn, Empty, Occupied Red Pawn, Empty, Occupied Red Pawn, Empty]
+    , [Empty, Occupied Red Pawn, Empty, Occupied Red Pawn, Empty, Occupied Red Pawn, Empty, Occupied Red Pawn]
+    , [Occupied Red Pawn, Empty, Occupied Red Pawn, Empty, Occupied Red Pawn, Empty, Occupied Red Pawn, Empty]
+    ]
+
+initialGameState :: Player -> GameState
+initialGameState startPlayer = GameState
+    { board = initialBoard
+    , currentPlayer = startPlayer
+    , gameOver = False
+    , winner = Nothing
+    }
+
+showSquare :: Square -> String
+showSquare Empty = " "
+showSquare (Occupied Red Pawn) = "r"
+showSquare (Occupied Red King) = "R"
+showSquare (Occupied Black Pawn) = "b"
+showSquare (Occupied Black King) = "B"
+
+showBoard :: Board -> String
+showBoard board = 
+    "  0 1 2 3 4 5 6 7\n" ++
+    intercalate "\n" (zipWith showRow [0..] board) ++
+    "\n"
+  where
+    showRow i row = show i ++ " " ++ intercalate " " (map showSquare row)
 
 main :: IO ()
 main = putStrLn "Checkers game - starting..."
